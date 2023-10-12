a5debcf987874dd65725e246389ef94bce477696
NOOKALA DIVYA DURGA VEERA SAI AVINASH <110119399+21A35A0506@users.noreply.github.com>
2023-10-12 11:43:43 +0530

    Add files via upload

---
+++ github/HTML/validationJS.js
@@ -0,0 +1,22 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    
+    if (validateUser(username, password)) {
+        
+        window.location.href = 'StudentDetails.html';
+    } else {
+        
+        alert('Invalid username or password. Please try again.');
+    }
+});
+
+
+function validateUser(username, password) {
+    
+    return username === 'project@tcsfrt23.com' && password === 'password123';
+}
